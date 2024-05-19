8c31a12cf0c7c206bbb295d6aa8331b72d19e9f2
IceShuttle <IceShuttle@Debian>
2024-05-19 06:02:56 +0530

    cleaned_main

--- backend/server/src/main.rs
+++ backend/server/src/main.rs
@@ -16,8 +16,36 @@ use tower_http::services::ServeDir;
 
 type SharedState = Arc<RwLock<AppState>>;
 
-async fn create_state() -> AppState {
+#[tokio::main]
+async fn main() {
     dotenv().ok();
+    tracing_subscriber::fmt::init();
+
+    let app = create_router().await;
+
+    let port = env::var("PORT").expect("PORT not found");
+    println!("Listening on port: {port}");
+    let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{port}"))
+        .await
+        .unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn create_router() -> Router {
+    let state = Arc::new(RwLock::new(create_state().await));
+    Router::new()
+        .route("/new/user", post(auth::signup::create_user))
+        .route("/otp", post(auth::signup::send_otp))
+        .route("/login", post(auth::login::login))
+        .route("/users/:path/info", get(user::get_user))
+        .route("/pic", post(user::change_profile_pic))
+        .route("/posts/:id", get(post::get_post))
+        .route("/users/:path/posts", get(user::get_posts))
+        .with_state(Arc::clone(&state))
+        .nest_service("/res", ServeDir::new("res"))
+}
+
+async fn create_state() -> AppState {
     let db_url = env::var("DATABASE_URL").expect("DATABASE_URL not found");
     let pool = PgPoolOptions::new()
         .max_connections(5)
@@ -33,29 +61,6 @@ async fn create_state() -> AppState {
         pool,
         mail_pass,
         otp_storage,
-        jwt_secret_key: b"secretsecretsecretsecretsecretse".to_owned(),
+        jwt_secret_key: b"secretsecretsecretsecretsecretse".to_owned(), //Just for Testing
     }
-}
-
-#[tokio::main]
-async fn main() {
-    let state = Arc::new(RwLock::new(create_state().await));
-    let port = env::var("PORT").expect("PORT not found");
-
-    tracing_subscriber::fmt::init();
-    let app = Router::new()
-        .route("/new/user", post(auth::signup::create_user))
-        .route("/otp", post(auth::signup::send_otp))
-        .route("/login", post(auth::login::login))
-        .route("/users/*path", get(user::get_user))
-        .route("/pic", post(user::change_profile_pic))
-        .route("/posts/:id", get(post::get_post))
-        .with_state(Arc::clone(&state))
-        .nest_service("/res", ServeDir::new("res"));
-
-    println!("Listening on port: {port}");
-    let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{port}"))
-        .await
-        .unwrap();
-    axum::serve(listener, app).await.unwrap();
 }
