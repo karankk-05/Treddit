9c4ba8cf90ae3f76ff13bb76c2d4a8b461354c89
IceShuttle <IceShuttle@linux>
2024-07-17 01:24:11 +0530

    Using weighted search

--- server/migrations/20240716162631_search_text_column.up.sql
+++ server/migrations/20240716162631_search_text_column.up.sql
@@ -1,9 +1,7 @@
 begin;
 alter table posts add column text_search tsvector generated always as  
 (
-  case
-  when body is null then to_tsvector('english',title)
-  else to_tsvector('english',title || ' ' || body)
-  end
-) stored;
+  setweight(to_tsvector('english',title),'A') || setweight(to_tsvector('english',coalesce(body,'')),'B')
+) 
+stored;
 commit;
