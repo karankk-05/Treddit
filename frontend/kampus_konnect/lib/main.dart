631aebbf986e4bd23e7e47b616cb0b865c7eed6d
karankk.05 <karank23@iitk.ac.in>
2024-07-29 01:28:01 +0530

    Added Search Function

--- frontend/kampus_konnect/lib/main.dart
+++ frontend/kampus_konnect/lib/main.dart
@@ -23,7 +23,7 @@ void main() async {
 }
 
 class MyApp extends StatefulWidget {
-  static const String baseUrl = 'http://172.23.144.23:3000';
+  static const String baseUrl = 'http://172.23.149.47:3000';
   @override
   _MyAppState createState() => _MyAppState();
 }
